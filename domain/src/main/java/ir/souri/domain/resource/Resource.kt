77d0bc9c4e52b16a25e822e5aec9441d5ddc271a
Ehsan <ehsansouri23@gmail.com>
2020-10-05 14:40:35 +0330

    modify resource

--- domain/src/main/java/ir/souri/domain/resource/Resource.kt
+++ domain/src/main/java/ir/souri/domain/resource/Resource.kt
@@ -1,15 +1,26 @@
 package ir.souri.domain.resource
 
-open class Resource<T>(
+sealed class Resource<T>(
     val status: Status,
     val data: T? = null,
     val message: String? = null
 ) {
-    class Success<T>(data: T? = null) : Resource<T>(Status.SUCCESS, data)
-    class Loading<T>(data: T? = null) : Resource<T>(Status.LOADING, data)
-    class Error<T>(message: String = "", data: T? = null) : Resource<T>(Status.ERROR, data, message)
+    class Success<T>(data: T? = null) :
+        Resource<T>(Status.SUCCESS, data)
+
+    class Loading<T>(data: T? = null) :
+        Resource<T>(Status.LOADING, data)
+
+    class Error<T>(message: String? = "", data: T? = null) :
+        Resource<T>(Status.ERROR, data, message)
 }
 
-fun <I, O> Resource<I>.mapData(transformMethod: (I?) -> O): Resource<O> {
-    return Resource(status, transformMethod(data), message)
-}
+fun <I, O> Resource<I>.mapData(
+    transformMethod: (I) -> O
+): Resource<O> =
+
+    when (status) {
+        Status.ERROR -> Resource.Error(message, data?.let { transformMethod(it) })
+        Status.LOADING -> Resource.Loading(data?.let { transformMethod(it) })
+        Status.SUCCESS -> Resource.Success(data?.let { transformMethod(it) })
+    }
